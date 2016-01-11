cef8690ae36455de60daf55f1fe28cbc88d1fa5a
Risto Hinno <ristohinno@gmail.com>
2016-01-11 14:19:42 +0200

    lisasin 11.01.2016 andmed

--- server.R
+++ server.R
@@ -2,7 +2,7 @@ library(shinydashboard)
 source("helpers.R")
 library(ggplot2)
 
-andmed=readRDS("./andmed/2016-01-08_andmedPikk.rds")
+andmed=readRDS("./andmed/2016-01-11_andmedPikk.rds")
 andmed$ministeerium=gsub("i haldusala", "", andmed$ministeerium)
 # andmedLai=andmedSisse()
 # andmed=DataLong2(andmedLai)
